3fd28ef16bfe7edf9f51ba0f41c596c783002cac
Yannick HUGUES <yannhweb@gmail.com>
2021-02-14 17:55:24 +0100

    create apple

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const ctx = canvas.getContext('2d');
 
 // Variables
 
-// Vitesse sur x
-vx = 0;
-// Vitesse sur y
-vy = -10;
-
+// Speed x
+let vx = 10;
+// Speed y
+let vy = 0;
+// Apple X
+let appleX = 0;
+// Apple Y
+let appleY = 0;
 
 let snake = [{
     x: 140,
@@ -30,6 +33,7 @@ let snake = [{
 function animation() {
   setTimeout(function () {
     cleanCanvas();
+    drawApple();
     moveSnack();
     drawSnake();
     animation();
@@ -37,33 +41,33 @@ function animation() {
 }
 
 function cleanCanvas() {
-  
+
   ctx.fillStyle = "white";
   ctx.strokeStyle = "grey";
   ctx.fillRect(0, 0, canvas.width, canvas.height)
   ctx.strokeRect(0, 0, canvas.width, canvas.height)
-  
+
 }
 
 function drawPiece(piece) {
-  
+
   ctx.fillStyle = "#00fe14";
   ctx.strokeStyle = "black";
   ctx.fillRect(piece.x, piece.y, 10, 10);
   ctx.strokeRect(piece.x, piece.y, 10, 10);
-  
+
 }
 
 function drawSnake() {
-  
+
   snake.forEach(piece => {
     drawPiece(piece);
   })
-  
+
 }
 
 function moveSnack() {
-  
+
   const head = {
     x: snake[0].x + vx,
     y: snake[0].y + vy
@@ -71,9 +75,6 @@ function moveSnack() {
   snake.unshift(head);
   snake.pop();
 }
-
-drawSnake();
-animation();
 
 document.addEventListener('keydown', changeDirection);
 
@@ -90,9 +91,58 @@ function changeDirection(event) {
   const down = vy === 10;
   const right = vx === 10;
   const left = vx === -10;
-  
-  if (direction === KEY_LEFT && !right) { vx = -10; vy = 0; }
-  if (direction === KEY_RIGHT && !left) { vx = 10; vy = 0; }
-  if (direction === KEY_UP && !down) { vx = 0; vy = -10; }
-  if (direction === KEY_DOWN && !up) { vx = 0; vy = 10; }
+
+  if (direction === KEY_LEFT && !right) {
+    vx = -10;
+    vy = 0;
+  }
+  if (direction === KEY_RIGHT && !left) {
+    vx = 10;
+    vy = 0;
+  }
+  if (direction === KEY_UP && !down) {
+    vx = 0;
+    vy = -10;
+  }
+  if (direction === KEY_DOWN && !up) {
+    vx = 0;
+    vy = 10;
+  }
 }
+
+function randomApple() {
+
+  return Math.round((Math.random() * 290) / 10) * 10;
+
+}
+
+function createApple() {
+
+  appleX = randomApple();
+  appleY = randomApple();
+
+  console.log(appleX, appleY);
+
+  snake.forEach(function (piece) {
+    const snakeOnApple = piece.x == appleX && piece.y == appleY;
+    if (snakeOnApple) {
+      createApple();
+    }
+  })
+
+}
+
+function drawApple() {
+
+  ctx.fillStyle = 'red';
+  ctx.strokeStyle = "darkred";
+  ctx.beginPath();
+  ctx.arc(appleX + 5, appleY + 5, 5, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
+
+}
+
+drawSnake();
+animation();
+createApple();
